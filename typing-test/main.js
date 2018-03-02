a9759505078173bee03395cdbd77d1ffdc8a391d
Anthony Rodarte <anthonyrodarte1422@gmail.com>
2018-03-02 11:05:18 -0800

    changed styling

--- typing-test/main.js
+++ typing-test/main.js
@@ -8,6 +8,7 @@ function postPrompt(words, location, position) {
   var $words = words.text
 
   var $pre = document.createElement('span')
+  $pre.classList.add('pre')
   $pre.textContent = $words.slice(0, position)
 
   var $current = document.createElement('span')
@@ -15,6 +16,7 @@ function postPrompt(words, location, position) {
   $current.textContent = $words[position]
 
   var $next = document.createElement('span')
+  $next.classList.add('next')
   $next.textContent = $words.slice(position + 1)
 
   location.append($pre, $current, $next)
